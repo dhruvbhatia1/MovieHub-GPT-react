ff7a97cb5a8327822b4f7303bf830f710f30b8f5
Dhruv Bhatia <dbhatia03@gmail.com>
2023-09-08 15:35:50 +0530

    navigation bug fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { signOut } from "firebase/auth";
+import { signOut, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { addUser, removeUser } from "../utils/userSlice";
+import { useDispatch } from "react-redux";
 
 const Header = () => {
+	const dispatch = useDispatch();
 	const navigate = useNavigate();
 	const user = auth.currentUser;
 	const handleSignOut = () => {
 		signOut(auth)
-			.then(() => {
-				navigate("/")
-			})
+			.then(() => {})
 			.catch((error) => {
 				navigate("/error");
 			});
 	};
+
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				// User is signed in
+				const { uid, email, displayName } = user;
+				dispatch(
+					addUser({
+						uid: uid,
+						email: email,
+						displayName: displayName,
+					})
+				);
+				navigate("/browse");
+			} else {
+				// User is signed out
+				dispatch(removeUser());
+				navigate("/");
+			}
+		});
+	}, []);
+
 	return (
 		<div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
 			<img
@@ -22,16 +45,18 @@ const Header = () => {
 				src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 				alt="logo"
 			/>
-			{user && <div className="flex p-2">
-				<img
-					className="w-12 h-12"
-					src="https://occ-0-5264-2186.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e"
-					alt="user-icon"
-				/>
-				<button onClick={handleSignOut} className="font-bold text-white">
-					(Sign Out)
-				</button>
-			</div>}
+			{user && (
+				<div className="flex p-2">
+					<img
+						className="w-12 h-12"
+						src="https://occ-0-5264-2186.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e"
+						alt="user-icon"
+					/>
+					<button onClick={handleSignOut} className="font-bold text-white">
+						(Sign Out)
+					</button>
+				</div>
+			)}
 		</div>
 	);
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -7,14 +7,12 @@ import {
 	updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { addUser} from "../utils/userSlice";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
 	const [isSignInForm, setIsSignInForm] = useState(true);
 	const [errorMessage, setErrorMessage] = useState(null);
-	const navigate = useNavigate();
 	const dispatch = useDispatch();
 	const name = useRef(null);
 	const email = useRef(null);
@@ -49,7 +47,6 @@ const Login = () => {
 									displayName: displayName,
 								})
 							);
-							navigate("/browse");
 						})
 						.catch((error) => {
 							// An error occurred
@@ -75,8 +72,7 @@ const Login = () => {
 				.then((userCredential) => {
 					// Signed in
 					const user = userCredential.user;
-					console.log(user);
-					navigate("/browse");
+					// console.log(user);
 					// ...
 				})
 				.catch((error) => {
@@ -96,7 +92,7 @@ const Login = () => {
 			<div className="absolute">
 				<img
 					src="https://assets.nflxext.com/ffe/siteui/vlv3/42df4e1f-bef6-499e-87ff-c990584de314/5e7c383c-1f88-4983-b4da-06e14c0984ba/IN-en-20230904-popsignuptwoweeks-perspective_alpha_website_large.jpg"
-					alt="background-image"
+					alt="background"
 				/>
 			</div>
 			<form
